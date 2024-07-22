45de9c7808de1d4be9f1f99c5f095af8fcef11ba
Sedi98 <sedi9816@gmail.com>
2024-07-22 12:29:31 +0400

    added comments

--- script.js
+++ script.js
@@ -1,34 +1,28 @@
+// registration page credentials
 const inputs = document.querySelectorAll(".input");
 const nameInput = document.querySelector(".nameInput");
 const surnameInput = document.querySelector(".surnameInput");
 const mailInput = document.querySelector(".mailInput");
 const phoneInput = document.querySelector(".phoneInput");
 const errText = document.querySelectorAll(".errText");
-
+const btnSubmit = document.querySelector(".btnSubmit");
 // details page credentials
 const userNameText = document.querySelector(".userNameText");
 const usermailText = document.querySelector(".usermailText");
 const userphoneText = document.querySelector(".userphoneText");
-
-const btnSubmit = document.querySelector(".btnSubmit");
-
+// if error get true or false
 let isError = false;
 
-btnSubmit.addEventListener("click", (e) => {
-  isError = false;
-  e.preventDefault();
-  trimmerFunction();
-  checkInputs();
-  takeInfo();
-  resetInputs();
-});
 
+
+// trim all inputs values before submit
 function trimmerFunction() {
   for (let i = 0; i < inputs.length; i++) {
     inputs[i].value = inputs[i].value.trim();
   }
 }
 
+// check if inputs are empty and if empty display error
 function checkInputs() {
   for (let i = 0; i < inputs.length; i++) {
     if (inputs[i].value === "") {
@@ -39,7 +33,7 @@ function checkInputs() {
     }
   }
 }
-
+// if all inputs are not empty and isError is false take info
 function takeInfo() {
   if (!isError) {
     submitInfo(
@@ -51,6 +45,8 @@ function takeInfo() {
   }
 }
 
+
+// create a object and submit data
 function submitInfo(nameValue, surnameValue, mailValue, phoneValue) {
   let obj = {
     name: nameValue,
@@ -63,6 +59,8 @@ function submitInfo(nameValue, surnameValue, mailValue, phoneValue) {
   window.location.href = "./profileInfo.html";
 }
 
+
+// after successfully submit then reset inputs
 function resetInputs() {
   if (!isError) {
     for (let i = 0; i < inputs.length; i++) {
@@ -72,6 +70,8 @@ function resetInputs() {
   }
 }
 
+
+// render all info on next page
 function renderInfo() {
   let info = JSON.parse(localStorage.getItem("info"));
   if (info) {
@@ -80,3 +80,15 @@ function renderInfo() {
     userphoneText.innerHTML = info.phone;
   }
 }
+
+
+
+// do all of this functions when button is clicked
+btnSubmit.addEventListener("click", (e) => {
+  isError = false;
+  e.preventDefault();
+  trimmerFunction();
+  checkInputs();
+  takeInfo();
+  resetInputs();
+});
